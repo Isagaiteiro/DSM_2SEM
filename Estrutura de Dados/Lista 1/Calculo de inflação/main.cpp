e5ca70e1dba0d5b4e77d2b7f6317bd97caa0b8bc
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2022-08-17 09:29:01 -0300

    Programa que atualiza o valor de um vetor
    Calcula 15% de inflação sobre 10 produtos de uma loja.

---
+++ Estrutura de Dados/Lista 1/Calculo de inflação/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+
+//Os preços dos produtos de uma loja serão atualizados por conta da inflação que aumentou 15%. 
+//Crie um  programa  que  vai popular  o  vetor de  10  unidades  de double depois vai atualizar os preços de acordo com a inflação. 
+//Depois disso, exibir o valor na tela.
+
+int main(){
+    setlocale(LC_ALL, "portuguese");
+    double produtos[10];
+    int x=1;
+    
+    //Repetição para preenchimento do vetor
+    for(int i=0; i<=2;i++){
+        cout<<"Digite o valor do "<< x << "° produto:\n";
+        cin>>produtos[i];
+        x++;
+    }
+    cout<<"---------------------\n";
+    
+    //Reseta o valor de x
+    x=1;
+    
+    cout<<"Preço Antigo\n";
+    cout<<"---------------------\n";
+    
+    //Repetição para imprimir o valor antigo na tela
+    for(int i=0; i<=2;i++){
+        cout<< x <<"° Produto "<< " : "<<produtos[i]<<endl;
+        x++;
+    }
+    cout<<"---------------------\n";
+    
+    //Reseta o valor de x
+    x=1;
+    //
+    cout<<"Preço Atualizado\n";
+    cout<<"---------------------\n";
+    for(int i=0; i<=2;i++){
+        produtos[i]=produtos[i]*1.15;
+        cout<< x <<"° Produto "<< " : "<<produtos[i]<<endl;
+        x++;
+    }
+    cout<<"---------------------\n";
+    
+}
