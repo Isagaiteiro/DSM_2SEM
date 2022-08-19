70b4195e82bb4e38e41f192231bbc16a4a8afb98
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2022-08-19 17:13:06 -0300

    Update main.cpp

--- Estrutura de Dados/Lista 1/Validação de resposta/main.cpp
+++ Estrutura de Dados/Lista 1/Validação de resposta/main.cpp
@@ -11,65 +11,31 @@ using namespace std;
 //Faça a verificação para que o usuário não estrague o seu código.
 
 //Cabeçalho
-int respsim(string resposta);
-int respnao(string resposta);
+int resp(string resposta);
 
 //Função principal.
 int main(int argc, char** argv){
     setlocale(LC_ALL, "Portuguese");
-    string resposta, resp2;
-    
-    //laço de repetição da função principal do programa.
-    do{
+    string resposta;
         
     //Pergunta principal.
-    cout<<"Olá, o seu dia foi bom?\n";
+    cout<<"Digite Sim ou Não: \n";
     cin>>resposta;
     
-        
-        //Chamando a função de resposta sim.
-        if(resposta=="Sim" || resposta=="SIM" || resposta=="sim"){
-            respsim(resposta);
-        } 
-        
-        //Chamando a função de resposta não.
-        if(resposta=="Não" || resposta=="NÃO" || resposta=="não"){
-            respnao(resposta);
-        } 
-        
-        //Pergunta final para dar inicio ao laço de repetição.
-        cout<<"Deseja responder novamente?\n";
-        cin>>resp2;
-    //Condições do laço de repetição.
-    }while(resp2=="Sim" || resp2=="SIM" || resp2=="sim");
-    
-    //Mensagem de finalização do programa.
-    cout<<"Obrigado pro usar nosso programa.\n Tenha um ótimo dia!!";
-    
+    //Chamando a função para motrar a resposta.
+    cout<<resp(resposta);
 
     return 0;
 }
 
-//Função de resposta não.
-int respnao(string resposta){
+//Função de resposta.
+int resp(string resposta){
     
-    //condição para retorno do resultado.
+    //condição para retorno.
     if(resposta=="Não" || resposta=="NÃO" || resposta=="não"){
-        cout<<"Espero que melhore!\n";
-    } else {
-        cout<<"Essa resposta não é valida!\n";
+        return 0;
     }
-    return 0;
-}
-
-//Função de resposta sim.
-int respsim(string resposta){
-    
-    //condição para retorno do resultado.
     if(resposta=="Sim" || resposta=="SIM" || resposta=="sim"){
-        cout<<"Que bom, deu tudo certo!\n";
-    } else {
-        cout<<"Essa resposta não é valida!\n";
+        return 1;
     }
-    return 1;
 }
