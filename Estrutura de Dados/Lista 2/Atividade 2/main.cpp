d9104ca81a027083d68812bbb74d62e1dd27d710
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2022-09-02 10:02:51 -0300

    Update main.cpp

--- Estrutura de Dados/Lista 2/Atividade 2/main.cpp
+++ Estrutura de Dados/Lista 2/Atividade 2/main.cpp
@@ -27,10 +27,13 @@ struct pessoa {
 
 int main(){
     
-    int cont=0;
+    int i, j;
     struct pessoa p[5];
+    int aux_dia, aux_mes, aux_ano;
+    char aux_nome[30];
+    
     //Popule um vetor (através do usuário) que tenha 5 unidades
-    for(int i=0; i<2; i++){
+    for(i=0; i<5; i++){
         cout<<"Digite o nome: \n";
         cin>>p[i].nome;
         cout<<"Digite a data de nascimento \n";
@@ -42,32 +45,81 @@ int main(){
         cin>>p[i].data_nasc.ano;
         cout<<"----------------------------\n";
     }
+    
     //Imprimindo lista de pessoas na tela
-    for(int i=0; i<2; i++){
+    for(i=0; i<5; i++){
         cout<<p[i].nome<<endl;
         cout<<p[i].data_nasc.dia<<" / "<<p[i].data_nasc.mes<<" / "<<p[i].data_nasc.ano<<endl;
         cout<<"-----------\n";
     }
     
     //Busque a pessoa mais velha
-    for(int i=0; i<2; i++){
-        for(int j=0; i<2; i++){
-            if(p[i].data_nasc.ano<p[j].data_nasc.ano){
-                cont=i;
-                if(p[i].data_nasc.mes<p[j].data_nasc.mes){
-                    cont=i;
-                    if(p[i].data_nasc.dia<p[j].data_nasc.dia){
-                        cont=i;
+    for(i=0; i<5; i++){
+        for(j=0; i<5; i++){
+            if(p[i].data_nasc.dia<p[j].data_nasc.dia){
+                
+                aux_dia=p[j].data_nasc.dia;
+                p[j].data_nasc.dia=p[i].data_nasc.dia;
+                p[i].data_nasc.dia=aux_dia;
+                
+                aux_nome=p[i].nome;
+                p[i].nome=p[j].nome;
+                p[j].nome=aux_nome;
+                
+                aux_mes=p[j].data_nasc.mes;
+                p[j].data_nasc.mes=p[i].data_nasc.mes;
+                p[i].data_nasc.mes=aux_mes;
+                
+                aux_ano=p[j].data_nasc.ano;
+                p[j].data_nasc.ano=p[i].data_nasc.ano;
+                p[i].data_nasc.ano=aux_ano;
+                    
+                if(p[i].data_nasc.mes>p[j].data_nasc.mes){
+                    
+                    aux_mes=p[j].data_nasc.mes;
+                    p[j].data_nasc.mes=p[i].data_nasc.mes;
+                    p[i].data_nasc.mes=aux_mes;
+                    
+                    aux_nome=p[i].nome;
+                    p[i].nome=p[j].nome;
+                    p[j].nome=aux_nome;
+                    
+                    aux_dia=p[j].data_nasc.dia;
+                    p[j].data_nasc.dia=p[i].data_nasc.dia;
+                    p[i].data_nasc.dia=aux_dia;
+                    
+                    aux_ano=p[j].data_nasc.ano;
+                    p[j].data_nasc.ano=p[i].data_nasc.ano;
+                    p[i].data_nasc.ano=aux_ano;
+                    
+                    if(p[i].data_nasc.ano>p[j].data_nasc.ano){
+                        
+                        aux_ano=p[j].data_nasc.ano;
+                        p[j].data_nasc.ano=p[i].data_nasc.ano;
+                        p[i].data_nasc.ano=aux_ano;
+                           
+                        aux_nome=p[i].nome;
+                        p[i].nome=p[j].nome;
+                        p[j].nome=aux_nome;
+                           
+                        aux_dia=p[j].data_nasc.dia;
+                        p[j].data_nasc.dia=p[i].data_nasc.dia;
+                        p[i].data_nasc.dia=aux_dia;
+                        
+                        aux_mes=p[j].data_nasc.mes;
+                        p[j].data_nasc.mes=p[i].data_nasc.mes;
+                        p[i].data_nasc.mes=aux_mes;
+                       
                     }
                     
                 }
             }
         }
     }
-    cout<<cont;
     //Exiba o nome e a data de nascimento dessa pessoa mais velha
-    cout<<p[cont].nome<<endl;
-    cout<<p[cont].data_nasc.dia<<" / "<<p[cont].data_nasc.mes<<" / "<<p[cont].data_nasc.ano<<endl;
+    cout<<"A pessoa mais velha é: \n";
+    //cout<<p[cont].nome<<endl;
+    //cout<<p[cont].data_nasc.dia<<" / "<<p[cont].data_nasc.mes<<" / "<<p[cont].data_nasc.ano<<endl;
     cout<<"-----------\n";
    
     
