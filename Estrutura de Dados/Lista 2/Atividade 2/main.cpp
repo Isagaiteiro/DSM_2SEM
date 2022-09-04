efe48a1c77913bb5e4830df45c594347d32e4a43
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2022-09-04 08:48:07 -0300

    Completo

--- Estrutura de Dados/Lista 2/Atividade 2/main.cpp
+++ Estrutura de Dados/Lista 2/Atividade 2/main.cpp
@@ -27,72 +27,34 @@ struct pessoa {
 
 int main(){
     
-    int i, j;
-    struct pessoa p[5], aux;
+  
+    struct pessoa p[5];
+    int aux_ano, aux_mes, aux_dia;
     char aux_nome[30];
     
+    
     //Popule um vetor (através do usuário) que tenha 5 unidades
-    for(i=0; i<5; i++){
+    for(int i=0; i<5; i++){
+        
         cout<<"Digite o nome: \n";
         cin>>p[i].nome;
+    
         cout<<"Digite a data de nascimento \n";
         cout<<"Dia: \n";
         cin>>p[i].data_nasc.dia;
+        
         cout<<"Mês: \n";
         cin>>p[i].data_nasc.mes;
+        
         cout<<"Ano: \n";
         cin>>p[i].data_nasc.ano;
         cout<<"----------------------------\n";
     }
     
-    //Imprimindo lista de pessoas na tela
-    for(i=0; i<5; i++){
-        cout<<p[i].nome<<endl;
-        cout<<p[i].data_nasc.dia<<" / "<<p[i].data_nasc.mes<<" / "<<p[i].data_nasc.ano<<endl;
-        cout<<"-----------\n";
-    }
-    
     //Busque a pessoa mais velha
-    for(int i=0; i<5; i++){
-        for(int j=0; i<5; i++){
-            if(p[i].data_nasc.ano<p[j].data_nasc.ano){
-                
-                strcpy(aux.nome,p[j].nome);
-                aux.data_nasc.dia=p[j].data_nasc.dia;
-                aux.data_nasc.mes=p[j].data_nasc.mes;
-                aux.data_nasc.ano=p[j].data_nasc.ano;
-                
-                strcpy(p[j].nome,p[i].nome);
-                p[j].data_nasc.dia=p[i].data_nasc.dia;
-                p[j].data_nasc.mes=p[i].data_nasc.mes;
-                p[j].data_nasc.ano=p[i].data_nasc.ano;
-                
-                strcpy(p[i].nome,aux.nome);
-                p[i].data_nasc.dia=aux.data_nasc.dia;
-                p[i].data_nasc.mes=aux.data_nasc.mes;
-                p[i].data_nasc.ano=aux.data_nasc.ano;
-            }
-            
-            /*
-            if(p[i].data_nasc.mes>p[j].data_nasc.mes){
-                
-                strcpy(aux.nome,p[j].nome);
-                aux.data_nasc.dia=p[j].data_nasc.dia;
-                aux.data_nasc.mes=p[j].data_nasc.mes;
-                aux.data_nasc.ano=p[j].data_nasc.ano;
-                
-                strcpy(p[j].nome,p[i].nome);
-                p[j].data_nasc.dia=p[i].data_nasc.dia;
-                p[j].data_nasc.mes=p[i].data_nasc.mes;
-                p[j].data_nasc.ano=p[i].data_nasc.ano;
-                
-                strcpy(p[i].nome,aux.nome);
-                p[i].data_nasc.dia=aux.data_nasc.dia;
-                p[i].data_nasc.mes=aux.data_nasc.mes;
-                p[i].data_nasc.ano=aux.data_nasc.ano;
-            }
-            
-            if(p[i].data_nasc.dia>p[j].data_nasc.dia){
+   for (int i=0; i<5; i++){
+		for (int j=i+1; j<5; j++) {
+			if (p[i].data_nasc.ano > p[j].data_nasc.ano && p[i].data_nasc.mes > p[j].data_nasc.mes &&p[i].data_nasc.dia > p[j].data_nasc.dia){
                 
                 strcpy(aux_nome,p[j].nome);
                 aux_dia=p[j].data_nasc.dia;
@@ -108,20 +70,15 @@ int main(){
                 p[i].data_nasc.dia=aux_dia;
                 p[i].data_nasc.mes=aux_mes;
                 p[i].data_nasc.ano=aux_ano;
-            }
-            */
+            
+            }    
         }
     }
-    cout<<"Lista ordenada: \n";
-    for(i=0; i<5; i++){
-        cout<<p[i].nome<<endl;
-        cout<<p[i].data_nasc.dia<<" / "<<p[i].data_nasc.mes<<" / "<<p[i].data_nasc.ano<<endl;
-        cout<<"-----------\n";
-    }
+
     //Exiba o nome e a data de nascimento dessa pessoa mais velha
     cout<<"A pessoa mais velha é: \n";
     cout<<p[0].nome<<endl;
-    cout<<p[0].data_nasc.dia<<" / "<<p[0].data_nasc.mes<<" / "<<p[0].data_nasc.ano<<endl;
+    cout<<p[0].data_nasc.dia<<"/"<<p[0].data_nasc.mes<<"/"<<p[0].data_nasc.ano<<endl;
     cout<<"-----------\n";
    
     
