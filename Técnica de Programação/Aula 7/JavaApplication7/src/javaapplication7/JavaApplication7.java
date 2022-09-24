a4d737e5024152fc42e1a957b25ec6b053d83d85
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2022-09-23 21:47:55 -0300

    Update JavaApplication7.java

--- Técnica de Programação/Aula 7/JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ Técnica de Programação/Aula 7/JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -4,6 +4,8 @@ public class JavaApplication7 {
 
    
     public static void main(String[] args) {
+       //Instanciar o Carro, usando atributos da SUPERCLASSE.
+       Carro c = new Carro(1,4,"Azul", 4, 2);
        
     }
     
