b99a369181f8253300051f726947da903a6aea5f
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2022-11-05 09:58:23 -0300

    Add files via upload

--- Estrutura de Dados/Lista 4/Atividade 3/main.cpp
+++ Estrutura de Dados/Lista 4/Atividade 3/main.cpp
@@ -1,77 +1,70 @@
 /******************************************************************************
 
-3)Faça um programa de estoque de loja:
-a)Crie uma struct PRODUTO que possua um CODIGO(inteiro),
-um NOME(char) e um PRECO(real). Ao criar esse novo tipo, crie variáveis ponteiros 
-para essa estrutura.
-b)Usando alocação de memória, crie um vetor de 5 elementos, porém criando 1 por 1 
-dos elementosdo vetor. Faça o cadastro de 5 produtos.
+3) Faça um programa de estoque de loja:
+
+a)Crie uma struct PRODUTO que possua um CODIGO(inteiro),uma NOME(char)e um PRECO(real).
+Ao criar esse novo tipo, crie variáveis ponteiros para essa estrutura.
+b)Usando alocação de memória,crie um vetor de 5 elementos,porém criando 1 por 1 dos elementos do vetor. 
+Faça o cadastro de 5 produtos.
 c)Ordene os produtos de acordo com o preço do produto. Ordene de forma decrescente.
 d)Imprima a média do valor dos preço dos produtos.
-e) Imprima todos os produtos.
+e)Imprima todos os produtos.
 
 *******************************************************************************/
-#include <stdio.h>
 #include <iostream>
-#include <cstdlib>
+#include <locale>
+
 
 using namespace std;
 
-/*
-a)Crie uma struct PRODUTO que possua um CODIGO(inteiro),
-um NOME(char) e um PRECO(real). Ao criar esse novo tipo, crie variáveis ponteiros 
-para essa estrutura.
-*/
-struct produto{
-    char nome[30];
-    int *codigo;
-    double *preco;
+
+struct produtos{
+	int codigo;
+	char nome[20];
+	float preco;
 };
 
-//c)Ordene os produtos de acordo com o preço do produto. Ordene de forma decrescente.
 
-void ordenar(){
+float mediaP (produtos *p);		
+
+int main(int argc, char** argv) {
+    produtos *p;
+	setlocale(LC_ALL,"Portuguese");
+	float *nome,*preco,*id;
+	
+	
+	p = (produtos *) malloc (5 * sizeof(float));
+	
+	for(int i=0; i<5;i++){
+	    p[i].codigo = i+1;
+		cout << "Insira o nome do produto:\n";
+		cin >> p[i].nome;
+		cout << "Insira o preço do produto:\n";
+		cin >> p[i].preco;	
+	}
     
+    cout << "==============================================\n";
+	cout << "------------------Produtos--------------------\n";
+	cout << "==============================================\n";	
+	
+	for(int i=0;i<5; i++){
+	    cout<<"Código: "<<p[i].codigo<<endl;
+	    cout<<"Produto: "<<p[i].nome<<endl;
+	    cout<<"Preço: "<<p[i].preco<<endl;
+	    cout<<"============================================\n";
+	}
+	
+	cout << "==============================================\n";
+	cout << "Média dos preços é de, R$" << mediaP(p)<<".\n";
+	cout << "==============================================\n";	
+	return 0;
 }
 
-int main()
-{
-    setlocale (LC_ALL, "Portuguese");
-    produto *p=NULL;
-    int tam=5, cod[5];
-    double vlr[5];
-    
-    //b)Usando alocação de memória, crie um vetor de 5 elementos. 
-    p=(produto*)malloc(tam* sizeof(produto));
-    
-    //criando 1 por 1 dos elementosdo vetor. Faça o cadastro de 5 produtos. 
-    for(int i=0; i<5; i++){
-        
-        cout<<"Digite o código do "<<i+1<<"º produto:\n";
-        p[i].codigo=&cod[i];
-        cin>>*(p[i].codigo);
-        
-        cout<<"Digite o nome do "<<i+1<<"º produto:\n";
-        cin>>p[i].nome;
-        
-        cout<<"Digite o preço do "<<i+1<<"º produto:\n";
-        p[i].preco=&vlr[i];
-        cin>>*(p[i].preco);
-    }
-    
-    //e) Imprima todos os produtos.
-    for(int i=0; i<5; i++){
-            cout<<"============================\n";
-            cout<<"-----------Estoque----------\n";
-            cout<<*(p[i].codigo)<<endl;
-            cout<<p[i].nome<<endl;
-            cout<<*(p[i].preco)<<endl;
-            cout<<"-------------------------\n";
-    }
-
-    return 0;
-}
-
-
-
-
+float mediaP (produtos *p){
+	float total = 0, media;
+	for(int i=0;i<5;i++){
+		total = total + p[i].preco;	
+		media = total/5;
+	}
+	return media;
+};
