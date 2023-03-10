f5dabaa319fb86bb0706cf615f8107932d820157
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2023-03-09 21:55:42 -0300

    Teste de subconsulta dentro do case

--- Banco de Dados Relacional/Atividade 3.sql
+++ Banco de Dados Relacional/Atividade 3.sql
@@ -1,1 +1,5 @@
+/*Realizando consulta com case*/
 SELECT c.id, c.nome, case when tipo = "F" then "Pessoa Fisica" when tipo = "J" then "Pessoa Juridica" ELSE "Sem Classificacao" END AS tipo FROM cliente c
+
+/*Usando subconsulta dentro do case*/
+SELECT c.id, c.nome, case when tipo = "F" then (SELECT r.nome FROM  representante r WHERE r.id = c.idrepresentante) when tipo = "J" then "Pessoa Juridica" ELSE "Sem Classificacao" END AS tipo FROM cliente c
